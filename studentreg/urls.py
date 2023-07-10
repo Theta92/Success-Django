f0da011542075b83734906d4199508914d14c16b
Theta92 <onyekaoshoba@gmail.com>
2023-07-10 17:19:31 +0000

    Home edited

--- studentreg/urls.py
+++ studentreg/urls.py
@@ -5,7 +5,10 @@ app_name = 'studentreg'
 urlpatterns = [
 
 path('', views.home, name = 'home'),
-path('about', views.about, name='about'),
-path('contact', views.contact, name='contact'),
+path('about/', views.about, name='about'),
+path('contact/', views.contact, name='contact'),
+path('modules/', views.modules, name='modules'),
+
+
 
 ]
--- studentreg/views.py
+++ studentreg/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'studentreg/home.html')
+    return render(request, 'studentreg/home.html', {'title': 'Welcome'})
 def about(request):
-    return render(request, 'studentreg/about.html')
+    return render(request, 'studentreg/about.html', {'title': 'About'})
 def contact(request):
-    return render(request, 'studentreg/contact.html')
+    return render(request, 'studentreg/contact.html', {'title': 'Contact'})
+def modules(request):
+    return render(request, 'studentreg/modules.html', {'title': 'Modules'})
 
 # Create your views here.
