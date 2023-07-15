7c5a1d5001e5015b8cc697dae1ad1fdb55ddc5d9
Nisha-codes <eeshaothman@gmail.com>
2023-07-15 21:15:07 +0000

    student account created on user register

--- studentreg/apps.py
+++ studentreg/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class StudentregConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'studentreg'
+
+    def ready(self):
+        from .signals import create_student
