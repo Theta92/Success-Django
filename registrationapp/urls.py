2d32cd67ed40c82702d574ded5b4b0eb9eb0178e
Nisha-codes <eeshaothman@gmail.com>
2023-07-15 21:41:54 +0000

    login/logout feature

--- registrationapp/urls.py
+++ registrationapp/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 from studentreg import views
 
@@ -24,4 +27,14 @@ urlpatterns = [
     path("", include("studentreg.urls")),
     path("login/", views.login, name="login"),
     path("register/", views.register, name="register"),
-]
+    path(
+        "login",
+        auth_views.LoginView.as_view(template_name="studentreg/login.html"),
+        name="login",
+    ),
+    path(
+        "logout",
+        auth_views.LogoutView.as_view(template_name="studentreg/logout.html"),
+        name="logout",
+    ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
